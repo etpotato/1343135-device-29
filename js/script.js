53f494a995802cf0fd456c6bf8f8d6d1b447c1d0
etpotatoo <slyboot93@mail.ru>
2020-10-14 09:07:20 +0300

    Объединяет скрипты в один файл

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const linkMap = document.querySelector('.contacts-map');
+const popupMap = document.querySelector('.modal-wrap-map');
+const mapUnderlay = popupMap.querySelector('.modal-underlay-map');
+const mapClose = popupMap.querySelector('.button-close-map');
+
 const linkMail = document.querySelector('.info-link-mail');
 const mailWrap = document.querySelector('.modal-wrap-mail');
 const mailUnderlay = mailWrap.querySelector('.modal-underlay-mail');
@@ -12,10 +17,46 @@ const mailEmail = mailForm.querySelector('.mail-input-email');
 const mailMessage = mailForm.querySelector('.mail-input-message');
 const mailSubmit = mailForm.querySelector('.mail-submit');
 
+// Open map popup
+linkMap.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  popupMap.classList.add('modal-wrap-show');
+});
+
+// Close map popup with close button
+mapClose.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  popupMap.classList.remove('modal-wrap-show');
+});
+
+// Close map popup with click on underlay
+mapUnderlay.addEventListener('click', () => {
+  popupMap.classList.remove('modal-wrap-show');  
+});
+
+/* 
+// Вариант с делегированием
+popupMap.addEventListener('click', (evt) => {
+  if (evt.target === mapUnderlay || evt.target === mapClose) {
+    evt.preventDefault();
+    popupMap.classList.remove('modal-wrap-show');
+  }
+});
+*/
+
+// Close map popup with ESC
+window.addEventListener('keydown', (evt) => {
+  if (evt.keyCode === 27 || evt.keyCode === 'Escape') {
+    if (popupMap.classList.contains('modal-wrap-show')) {
+      popupMap.classList.remove('modal-wrap-show');  
+    }
+  }
+});
+
+// is localStorage supported?
 let storageName = '';
 let isStorageSupport = true;
 
-// is localStorage supported?
 try {
   storageName = localStorage.getItem('name');
 } catch (err) {
